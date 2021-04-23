587753cb67bee32382cf9df1d69cb25409d33c6f
Md. Nahid Hasan Limon <41167616+NahidHasanLimon@users.noreply.github.com>
2021-04-24 03:25:33 +0600

    Different Audio Play

---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:audioplayers/audio_cache.dart';
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  void playSound(int i){
+    final player = AudioCache();
+    player.play('note$i.wav');
+  }
+  Expanded buildExpnadedWidget({Color color , int soundNumber}){
+    return Expanded(
+      child:  FlatButton(
+        color: color,
+        onPressed: (){
+          playSound(soundNumber);
+        },
+
+      ),
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildExpnadedWidget(color: Colors.tealAccent, soundNumber: 1),
+              buildExpnadedWidget(color: Colors.redAccent, soundNumber: 2),
+              buildExpnadedWidget(color: Colors.deepPurpleAccent, soundNumber: 3),
+              buildExpnadedWidget(color: Colors.deepOrangeAccent, soundNumber: 4),
+              buildExpnadedWidget(color: Colors.lightBlue, soundNumber: 5),
+              buildExpnadedWidget(color: Colors.green, soundNumber: 6),
+              buildExpnadedWidget(color: Colors.orange, soundNumber: 7),
+
+
+
+            ],
+          ),
+
+
+
+
+
+        ),
+      ),
+    );
+  }
+}
